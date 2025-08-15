05a8e8a2398ade0551133c0f39fa6e039366bb10
akshxdevs <akshxdevs@gmail.com>
2025-08-15 18:57:03 +0530

    contract updated

--- programs/anchor-nftflashloan/src/lib.rs
+++ programs/anchor-nftflashloan/src/lib.rs
@@ -6,11 +6,71 @@ declare_id!("H6jEh2UbEn4y4LzjYi8ZB4WP7nPUMFVmcpZJ8rbwQepa");
 pub mod anchor_nftflashloan {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>,fees_bps:u16,merkle_root:Option<[u8;32]>) -> Result<()> {
+        let config = &mut ctx.accounts.config;
+
+        config.admin = ctx.accounts.admin.key();
+        config.bump = ctx.bumps.config;
+        config.paused = false;
+        config.fee_bps = fees_bps;
+        config.merkle_root = merkle_root;
+        Ok(())
+    }
+    pub fn set_fee_bps(ctx: Context<OnlyAdminMutConfig>, new_fee_bps: u16) -> Result<()> {
+        ctx.accounts.target.fee_bps = new_fee_bps;
+        Ok(())
+    }
+
+    pub fn set_merkle_root(
+        ctx: Context<OnlyAdminMutConfig>, 
+        merkle_root: Option<[u8; 32]>
+    ) -> Result<()> {
+        ctx.accounts.target.merkle_root = merkle_root;
+        Ok(())
+    }
+
+    pub fn set_paused(ctx: Context<OnlyAdminMutConfig>, paused: bool) -> Result<()> {
+        ctx.accounts.target.paused = paused;
         Ok(())
     }
 }
 
+#[account]
+pub struct Config{
+    pub admin:Pubkey,
+    pub fee_bps:u16,
+    pub merkle_root:Option<[u8;32]>,
+    pub bump:u8,
+    pub paused:bool
+}impl Config {
+    pub const SIZE:usize = 32 + 2 + 33 + 4 + 4;
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [b"config",admin.key().as_ref()],
+        payer = admin,
+        space = 8 + Config::SIZE,
+        bump
+    )]
+    pub config:Account<'info,Config>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct OnlyAdminMut<'info, T: AccountSerialize + AccountDeserialize + Owner + Clone> {
+    #[account(mut)]
+    pub target: Account<'info, T>,
+    pub admin: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct OnlyAdminMutConfig<'info> {
+    #[account(mut)]
+    pub target: Account<'info, Config>,
+    pub admin: Signer<'info>,
+}
--- tests/anchor-nftflashloan.ts
+++ tests/anchor-nftflashloan.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorNftflashloan } from "../target/types/anchor_nftflashloan";
-
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import CryptoJS from "crypto-js";
 describe("anchor-nftflashloan", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace
+    .anchorNftflashloan as Program<AnchorNftflashloan>;
+  const admin = provider.wallet as anchor.Wallet;
+  const whiteList: PublicKey[] = [
+    Keypair.generate().publicKey,
+    Keypair.generate().publicKey,
+    Keypair.generate().publicKey,
+  ];
 
-  const program = anchor.workspace.anchorNftflashloan as Program<AnchorNftflashloan>;
+  function hashLeaf(mint: PublicKey): string {
+    return CryptoJS.SHA256(mint.toBase58()).toString(CryptoJS.enc.Hex);
+  }
+  const leaves = whiteList.map(hashLeaf);
+  function merkleRoot(leaves: string[]): string {
+    let level = [...leaves];
 
+    while (level.length > 1) {
+      const nextLevel: string[] = [];
+      for (let i = 0; i < level.length; i += 2) {
+        const left = level[i];
+        const right = i + 1 < level.length ? level[i + 1] : level[i];
+        const combined = CryptoJS.SHA256(left + right).toString(
+          CryptoJS.enc.Hex
+        );
+        nextLevel.push(combined);
+      }
+      level = nextLevel;
+    }
+
+    return level[0]; // hex string root
+  }
+
+  const root = merkleRoot(leaves);
+  console.log("Merkle root:", root);
+  function hexToBytes(hex: string): Uint8Array {
+    const bytes = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < bytes.length; i++) {
+      bytes[i] = parseInt(hex.substr(i * 2, 2), 16);
+    }
+    return bytes;
+  }
+  const merkleRootBytes = hexToBytes(root);
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initialize(100, Array.from(merkleRootBytes))
+      .accounts({
+        admin: admin.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
